daa7cc8959d2b853c3f56ac3f6fc5e7db2098629
Carlos <ccontreras@carnegiescience.edu>
2026-04-23 12:11:34 -0400

    Set catalog column headers for LLAMAS correctly

--- src/magellan/scripts/magellan_catalog.py
+++ src/magellan/scripts/magellan_catalog.py
@@ -93,7 +93,7 @@ elif(inst=='LLAMAS'):
     angle = angle + rzp
     if(mode!='OFF' and mode!='EQU'):
         mode='EQU'
-    print('Object,RA,Dec,Equinox,RA_Motion_Rate[sec/year],Dec_Motion_Rate[\"/year],RotAngle,RotMode')
+    print('Object,RA,Dec,Coords_Epoch,RA_Motion_Rate[sec/year],Dec_Motion_Rate[\"/year],PA,RotMode')
     for i in range(len(target_name)):
         line='%s,%s,%s,2000.0,%.6f,%.6f,%.3f,%s'%(target_name[i],ra_coord[i],dec_coord[i],mr_ra[i],mr_dec[i],angle[i],mode)
         print(line)
